f6ea4a1cb5d5720ad1f1dd0386d915a42afd41ad
Phelipe Bistaffa <phelipegithub@gmail.com>
2026-08-05 11:51:06 -0400

    fix : front

--- Integracao-Pagamento-Front/src/components/Home.jsx
+++ Integracao-Pagamento-Front/src/components/Home.jsx
@@ -99,7 +99,7 @@ export default function Home() {
       </main>
 
       <footer className="home__foot">
-        <span>Feito por Phelipe Bistaffa</span>
+        <span><a href='https://github.com/phelipeGit'>Feito por PhelipeGit</a></span>
       </footer>
     </div>
   )
